fb4485c6ee69d4812655bce8e72e4b4351ba57af
Ekram Hossain <ekramhossain117@gmail.com>
2023-05-18 22:14:58 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Home></Home>
        <Outlet></Outlet>
        <Footer></Footer>
     </>
